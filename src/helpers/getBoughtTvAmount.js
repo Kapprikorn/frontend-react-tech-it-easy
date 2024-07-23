d0d2547b705f30d2805b19ef01d261233216ad06
Kapprikorn <sjors2202@hotmail.com>
2024-07-23 16:15:51 +0200

    Fix: added Best Selling TV to helper functions

--- src/helpers/getBoughtTvAmount.js
+++ src/helpers/getBoughtTvAmount.js
@@ -1,9 +1,10 @@
-import {inventory} from "../constants/inventory.js";
+import { bestSellingTv, inventory } from '../constants/inventory.js'
 
 export default function getBoughtTvAmount() {
   let tvAmount = 0;
   for (const Television of inventory) {
     tvAmount += Television.originalStock;
   }
+  tvAmount += bestSellingTv.originalStock;
   return tvAmount;
 }
--- src/helpers/getSoldTvAmount.js
+++ src/helpers/getSoldTvAmount.js
@@ -1,9 +1,13 @@
-import {inventory} from "../constants/inventory.js";
+import {
+    bestSellingTv,
+    inventory,
+} from '../constants/inventory.js'
 
 export default function getSoldTvAmount() {
     let tvAmount = 0;
     for (const Television of inventory) {
         tvAmount += Television.sold;
     }
+    tvAmount += bestSellingTv.sold;
     return tvAmount;
 }
